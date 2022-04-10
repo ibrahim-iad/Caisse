74c78dfb62ff3708b7c8eb2f81a18c247c94606a
ibroman <ibroman79@gmail.com>
2022-04-10 23:06:58 +0300

    Ajout du taux de remise au Panier. Le total sera recalculé. Classe Panier2 a été ajoutée.

--- Panier/Program.cs
+++ Panier/Program.cs
@@ -13,11 +13,17 @@ namespace Panier
             Produit p1 = new Produit("Lait", 150, 2);
             Produit p2 = new Produit("Riz", 200, 3);
             Produit p3 = new Produit("Fanta", 50, 1);
-            Panier panier = new Panier();
+            /*Panier panier = new Panier();
             panier.AjouterProduit(p1);
             panier.AjouterProduit(p2);
             panier.AjouterProduit(p3);
-            panier.Imprimer(); 
+            panier.Imprimer();*/
+            Panier2 panier = new Panier2(10);
+            panier.AjouterProduit(p1);
+            panier.AjouterProduit(p2);
+            panier.AjouterProduit(p3);
+            panier.Imprimer();
+            Console.WriteLine("Remise:" + panier.Remise());
             Console.WriteLine("Total:" + panier.Total());
             
             Console.ReadKey();
